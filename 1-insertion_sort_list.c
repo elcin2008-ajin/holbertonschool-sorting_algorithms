ea90ffa3150ad210a7a410de23364bbf4127ce4c
elcin2008-ajin <elcinmahmudov123elcin@gmail.com>
2026-02-04 12:06:51 +0000

    kk

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -8,38 +8,38 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *current, *tmp;
+	listint_t *current, *tmp;
 
-    if (!list || !(*list) || !(*list)->next)
-        return;
+	if (!list || !(*list) || !(*list)->next)
+		return;
 
-    current = (*list)->next;
+	current = (*list)->next;
 
-    while (current)
-    {
-        tmp = current;
-        while (tmp->prev && tmp->n < tmp->prev->n)
-        {
-            listint_t *prev = tmp->prev;
-            listint_t *next = tmp->next;
+	while (current)
+	{
+		tmp = current;
+		while (tmp->prev && tmp->n < tmp->prev->n)
+		{
+			listint_t *prev = tmp->prev;
+			listint_t *next = tmp->next;
 
-            /* Swap nodes */
-            if (prev->prev)
-                prev->prev->next = tmp;
-            tmp->prev = prev->prev;
+			/* Swap nodes */
+			if (prev->prev)
+				prev->prev->next = tmp;
+			tmp->prev = prev->prev;
 
-            tmp->next = prev;
-            prev->prev = tmp;
-            prev->next = next;
-            if (next)
-                next->prev = prev;
+			tmp->next = prev;
+			prev->prev = tmp;
+			prev->next = next;
+			if (next)
+				next->prev = prev;
 
-            /* Update head if needed */
-            if (!tmp->prev)
-                *list = tmp;
+			/* Update head if needed */
+			if (!tmp->prev)
+				*list = tmp;
 
-            print_list(*list);
-        }
-        current = current->next;
-    }
+			print_list(*list);
+		}
+		current = current->next;
+	}
 }
