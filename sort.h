23f9e85b28be01fe6287877b7072dbb06070b81e
elcin2008-ajin <elcinmahmudov123elcin@gmail.com>
2026-02-04 12:20:41 +0000

    KK

--- sort.h
+++ sort.h
@@ -1,23 +1,24 @@
 #ifndef SORT_H
 #define SORT_H
 
-#include <stdio.h>
-#include <stdlib.h>
+#include <stddef.h>
 
-/* Doubly linked list node struct */
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer
+ * @prev: Points to the previous element
+ * @next: Points to the next element
+ */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
-/* Function prototypes */
-
-/* print_array prototype */
 void print_array(const int *array, size_t size);
-
-/* Sorting algorithms prototypes */
+void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
