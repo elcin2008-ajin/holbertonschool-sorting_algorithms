d387642e66c8e8e3de909b02b941738781587f83
elcin2008-ajin <elcinmahmudov123elcin@gmail.com>
2026-02-04 12:15:13 +0000

    kk

--- sort.h
+++ sort.h
@@ -4,55 +4,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/**
- * struct listint_s - Doubly linked list node
- *
- * @n: Integer stored in the node
- * @prev: Pointer to the previous element
- * @next: Pointer to the next element
- */
-typedef struct listint_s
-{
-	int n;
-	struct listint_s *prev;
-	struct listint_s *next;
-} listint_t;
+/* ... digər typedef və funksiyalar ... */
 
-/* Function prototypes */
-
-/**
- * print_list - Prints all elements of a doubly linked list
- * @list: Pointer to the head of the list
- */
-void print_list(const listint_t *list);
-
-/**
- * print_array - Prints all elements of an array of integers
- * @array: Pointer to the array
- * @size: Number of elements
- */
-void print_array(const int *array, size_t size);
-
-/**
- * bubble_sort - Sorts an array of integers in ascending order
- * using the Bubble sort algorithm
- * @array: Pointer to the array
- * @size: Number of elements
- */
-void bubble_sort(int *array, size_t size);
-
-/**
- * insertion_sort_list - Sorts a doubly linked list using Insertion sort
- * @list: Pointer to the head of the list
- */
-void insertion_sort_list(listint_t **list);
-
-/**
- * selection_sort - Sorts an array of integers in ascending order
- * using the Selection sort algorithm
- * @array: Pointer to the array
- * @size: Number of elements
- */
-void selection_sort(int *array, size_t size);
+/* Quick sort prototype */
+void quick_sort(int *array, size_t size);
 
 #endif /* SORT_H */
