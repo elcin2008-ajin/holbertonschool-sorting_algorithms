8d4943148ca3a64076e31654663297859765fb6f
elcin2008-ajin <elcinmahmudov123elcin@gmail.com>
2026-02-04 11:59:16 +0000

    kk

---
+++ print_array.c
@@ -0,0 +1,19 @@
+#include "sort.h"
+
+/**
+ * print_array - prints an array of integers
+ * @array: array to print
+ * @size: number of elements
+ */
+void print_array(const int *array, size_t size)
+{
+    size_t i;
+
+    for (i = 0; i < size; i++)
+    {
+        if (i != 0)
+            printf(", ");
+        printf("%d", array[i]);
+    }
+    printf("\n");
+}
